826a107ccee270da75988b3a50062caaa82a0440
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2022-01-20 10:52:52 +0700

    Cập nhật nhỏ hàm strcmp

--- h00-ham-chuan/strcmp-demo.c
+++ h00-ham-chuan/strcmp-demo.c
@@ -5,7 +5,14 @@
   Hàm strcmp trả về giá trị < 0 nếu s1 nhỏ hơn s2,
              trả về giá giá trị > 0 nếu s1 lớn hơn s2,
              trả về 0 nếu s1 bằng s2.
-  Quan hệ thứ tự được xác định dựa trên mã số của từng ký tự trong chuỗi.
+  .
+  Lô-gic so sánh:
+  Các ký tự được so sánh song song từ trái sang phải. Quan hệ so sánh
+  được xác định khi gặp cặp ký tự khác nhau đầu tiên hoặc cả 2 đều là.
+  các ký tự null.
+  Trong trường hợp gặp 2 ký tự khác nhau (1 ký tự có thể là null) quan
+  hệ thứ tự được xác định dựa trên mã số của các ký tự trong bảng mã.
+  Trong trường hợp gặp 2 ký tự null 2 chuỗi được coi là bằng nhau.
 */
 
 #include <stdio.h>
@@ -20,10 +27,17 @@ int main(int argc, char *argv[]) {
     TEST(argv[1], argv[2]);
     return 1;
   }
-  TEST("a", "b");  // strcmp("a", "b") < 0
-  TEST("c", "b");  // > 0
+
+  // Trường hợp 1: Gặp 2 ký tự khác nhau
+  TEST("a", "b");  // strcmp("a", "b") < 0: 'a' < 'b'
+  TEST("aab", "b");  // < 0: 'a' < 'b'
+  TEST("c", "bef");  // > 0: 'c' > 'b'
+  TEST("ab", "a");  // > 0: 'b' > '\0'
+  TEST("a", "ab");  // < 0: '\0' < 'b'
+
+  // Trường hợp 2: Gặp 2 ký tự '\0' =>
+  //  cả 2 chuỗi có cùng độ dài và các ký tự đều bằng nhau.
   TEST("a", "a");  // == 0
-  TEST("ab", "a");  // > 0
-  TEST("ab", "b");  // < 0  - So sánh từng ký tự, từ trái sang phải
+  TEST("bbb", "bbb");  // == 0
   return 0;
 }
