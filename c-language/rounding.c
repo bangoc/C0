372868641c009495dd725cd7e546172fa4cf87c9
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2022-06-30 05:28:00 +0700

    Cập nhật giải thích cách làm tròn mặc định

--- c-language/rounding.c
+++ c-language/rounding.c
@@ -2,8 +2,8 @@
 #include <fenv.h>
 
 int main() {
-  // Mặc định làm tròn đến giá trị gần nhất,
-  // nhưng thiên về "số chẵn"
+  // Mặc định làm tròn đến giá trị gần nhất, ưu tiên "số chẵn"
+  // (bit thấp nhất = 0), bởi vì ổn định hơn từ góc độ toán học
   // http://www.gnu.org/software/libc/manual/html_node/Rounding.html
   printf("%.1f\n", (8.5 + 9.0) / 2);
   printf("%.1f\n", (8.5 + 10) / 2);
