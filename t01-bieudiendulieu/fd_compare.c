5ce96561c752941051f5348c07f0e189399a5f13
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2022-12-22 10:25:30 +0700

    Ví dụ so sánh số thực

---
+++ t01-bieudiendulieu/fd_compare.c
@@ -0,0 +1,10 @@
+/* (C) Nguyen Ba Ngoc 2022 */
+
+#include <stdio.h>
+
+int main() {
+  float x = 3.1415f;
+  double y = 3.1415;
+  printf("x = %.3f y = %.3f\n", x, y);
+  printf("%d\n", x == y);
+}
