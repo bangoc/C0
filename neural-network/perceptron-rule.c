782ce900de0d3df9f6ba8e82493001c4995e5ea7
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2026-03-24 20:48:22 +0700

    Tái cấu trúc mã nguồn perceptron-rule

--- neural-network/perceptron-rule.c
+++ neural-network/perceptron-rule.c
@@ -3,61 +3,66 @@
 Huấn luyện 1 perceptron sử dụng giá trị thô của sai số
 x0 --
       \w0
-       \ -->g(z) = z>=1?1:0 --> y
+       \ -->o = g(x0 * w0 + x1 * w1)
        /
       /w1
 x1 --
+
+Sử dụng hàm ngưỡng làm hàm kích hoạt g(x) = x >= 1? 1: 0;
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int activate(float v) {
-  return v >= 1.0? 1: 0;
+int activate(float x) {
+  return x >= 1.0? 1: 0;
 }
 
+float randw() {
+  return (rand() + 1.0) / (rand() + 1.0);
+}
+
+#define ITER 10
+#define R 1.5
+
 int main() {
-  float d[][2] = {
+  float xx[][2] = {
     {0, 0},
     {0, 1},
     {1, 0},
     {1, 1}
   };
-  int t[] = {
+  int tt[] = {
     0,
     0,
-    0,
-    1
+    1,
+    0
   };
-  if (sizeof(d) / sizeof(d[0]) != sizeof(t) / sizeof(t[0])) {
+  if (sizeof(xx) / sizeof(xx[0]) != sizeof(tt) / sizeof(tt[0])) {
     printf("Dữ liệu huấn luyện không hợp lệ.\n");
     return 0;
   }
-  const int k_samples = sizeof(t)/sizeof(t[0]);
-  const int k_iter = 10;
-  const float k_rate = 1.5;
+  const int k_samples = sizeof(tt)/sizeof(tt[0]);
 
   /* Khởi tạo ngẫu nhiên các trọng số lan truyền */
   srand(time(NULL));
-  float w[2];
-  for (int i = 0; i < 2; ++i) {
-      w[i] = (rand() + 1.0) / (rand() + 1.0);
-  }
+  float w[2] = {randw(), randw()};
 
   /* Huấn luyện */
-  for (int i = 0; i < k_iter; ++i) {
+  for (int iter = 0; iter < ITER; ++iter) {
     float e = 0;
-    for (int j = 0; j < k_samples; ++j) {
-      float *x = d[j];
-      int y = activate(x[0] * w[0] + x[1] * w[1]);
-      printf("iter %d: w0 = %.2f w1 = %.2f x0 = %.2f x1 = %.2f t = %d y = %d\n",
-            i + 1, w[0], w[1], x[0], x[1], t[j], y);
-      if (y != t[j]) {
-        w[0] += k_rate * (t[j] - y) * x[0];
-        w[1] += k_rate * (t[j] - y) * x[1];
+    for (int i = 0; i < k_samples; ++i) {
+      float *x = xx[i];
+      int t = tt[i];
+      int o = activate(x[0] * w[0] + x[1] * w[1]);
+      printf("iter %d: w0 = %.2f w1 = %.2f x0 = %.2f x1 = %.2f t = %d o = %d\n",
+            i + 1, w[0], w[1], x[0], x[1], t, o);
+      if (o != t) {
+        w[0] += R * (t - o) * x[0];
+        w[1] += R * (t - o) * x[1];
       }
-      e += (t[j] - y) * (t[j] - y);
+      e += (t - o) * (t - o);
     }
-    printf("e = %.2f\n", e);
+    printf("e = %.3f\n", e);
   }
 }
